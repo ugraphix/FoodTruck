31262da653c86cc5a5a6561eb26523359601184b
Gordana Minovska <ekogoca@gmail.com>
2017-01-27 17:00:33 -0800

    Reorganize HTML into a template div that is added to the form with jquery

--- index.php
+++ index.php
@@ -14,40 +14,41 @@
 <body>
 <h1>Welcome to FoodTruck</h1>
 <h3>Feeling hungry? We got you covered! Make your selection, order and relax</h3>
+<div id="template" class="hide">
+    <div class="singleItem">
+        <select class="item" name="items[]">
+            <option value="" disabled selected>I want..</option>
+            <option value="pizza">Pizza - $5.25</option>
+            <option value="drink">Drink - $2.55</option>
+            <option value="iceCream">Ice Cream - $3.50</option>
+        </select>
+        <input type="number" name="quantity[]" min="1" max="10" placeholder="How many?">
+        <input type="button" class="removeItem" value="-">
+        <div class="hide toppings pizza">
+            <p>Additional Toppings - $1 each</p>
+            <input type="checkbox" value="cheese">Cheese
+            <input type="checkbox" value="mushrooms">Mushrooms
+            <input type="checkbox" value="bacon">Bacon
+            <input type="checkbox" value="pesto">Pesto
+        </div>
+        <div class="hide toppings drink">
+            <p>Choose your drink - $2.50 each</p>
+            <input type="checkbox" value="pepsi">Pepsi
+            <input type="checkbox" value="coke">Coke
+            <input type="checkbox" value="coffee">Coffee
+            <input type="checkbox" value="tea">Tea
+        </div>
+        <div class="hide toppings iceCream">
+            <p>Additional Flavors - $.50 each</p>
+            <input type="checkbox" value="chocolate">Chocolate
+            <input type="checkbox" value="walnuts">Walnuts
+            <input type="checkbox" value="coconuts">Coconut
+            <input type="checkbox" value="strawberries">Strawberries
+        </div>
+    </div>
+</div>
 <form method="post" action="formhandler.php">
     <div id = "food">
-        <div class="singleItem">
-    <select class="item" name="items[]">
-        <option disabled selected>I want..</option>
-        <option value="pizza">Pizza - $5.25</option>
-        <option value="drink">Drink - $2.55</option>
-        <option value="iceCream">Ice Cream - $3.50</option>
-    </select>
-    <input type="number" name="quantity[]" min="1" max="10" placeholder="How many?">
-        </div>
-<!-- This is for the toppings. They would show after a selection of food is made
-(Extra Credit - @todo Include toppings after the basic app is finished-->
-<!--    <div class="pizzaToppings hide">-->
-<!--    <p>Additional Toppings - $1 each</p>-->
-<!--    <input type="checkbox" name="pizzaToppings[]" value="cheese">Cheese-->
-<!--    <input type="checkbox" name="pizzaToppings[]" value="mushrooms">Mushrooms-->
-<!--    <input type="checkbox" name="pizzaToppings[]" value="bacon">Bacon-->
-<!--    <input type="checkbox" name="pizzaToppings[]" value="pesto">Pesto-->
-<!--    </div>-->
-<!--    <div class=" drinks hide">-->
-<!--    <p>Choose your drink - $2.50 each</p>-->
-<!--    <input type="checkbox" name="drinks[]" value="pepsi">Pepsi-->
-<!--    <input type="checkbox" name="drinks[]" value="coke">Coke-->
-<!--    <input type="checkbox" name="drinks[]" value="coffee">Coffee-->
-<!--    <input type="checkbox" name="drinks[]" value="tea">Tea-->
-<!--    </div>-->
-<!--    <div class="iceCream hide">-->
-<!--    <p>Additional Flavors - $.50 each</p>-->
-<!--    <input type="checkbox" name="iceCream[]" value="chocolate">Chocolate-->
-<!--    <input type="checkbox" name="iceCream[]" value="walnuts">Walnuts-->
-<!--    <input type="checkbox" name="iceCream[]" value="coconuts">Coconut-->
-<!--    <input type="checkbox" name="iceCream[]" value="strawberries">Strawberries-->
-<!--    </div>-->
     </div>
     <input type="button" id="addItem" value="Add More">
     <input type="submit" value=" Place Order">

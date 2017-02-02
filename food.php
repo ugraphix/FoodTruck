46409a67854f4ef74e4981a76e89e191cbfcb7f4
Gordana Minovska <ekogoca@gmail.com>
2017-02-01 22:48:47 -0800

    Refactor food class, so all the data on the page comes from it. Include setProperties method that has a switch on the type of food.

--- food.php
+++ food.php
@@ -9,26 +9,70 @@
 class Food
 {
     //class fields
+    public $type;
     public $name;
     public $description;
     public $price;
     public $quantity;
+    //user selected additions
     public $toppings = array();
+    //available extras(toppings) per food type
+    public  $extras = array();
     //create class level property TAX
     public static $TAX = 0.9;
+    private static $TOPPINGS_FEE = 0.75;
 
     //constructor
-    public function __construct($name = '',$description = '',$quantity = 0,$price = 0, $toppings = array())
+    public function __construct($type,$quantity = 0,$toppings = array())
     {
-        $this->name = $name;
-        $this->description = $description;
+        $this->type = $type;
         $this->quantity = $quantity;
-        $this->price = $price;
         $this->toppings = $toppings;
+        $this->SetProperties($type);
     }
 
     //methods
+    /**
+     * @param $type
+     */
+    private function SetProperties($type){
+    switch ($type) {
+        case "pizza":
+            $this->name = "Italian Pizza";
+            $this->description ="Delicious taste of Italy,with fresh mozzarella and olive oil";
+            $this->price = 7;
+            $this->extras = ["Peppers", "Mushrooms", "Tomatoes", "Pesto"];
+            break;
+        case "burrito":
+            $this->name = "Mexican Burrito";
+            $this->description ="¡Ay, caramba! Straight from burrito heaven to your table";
+            $this->price = 5;
+            $this->extras = ["Cilantro", "Sour Cream", "Guacamole", "Salsa"];
+            break;
+        case "salad":
+            $this->name = "Greek Salad";
+            $this->description ="Fresh like a summer breeze over the Mediterranean";
+            $this->price = 4.5;
+            $this->extras = ["Olives", "Feta Cheese", "Oregano", "Onions"];
+            break;
+        case "curry":
+            $this->name = "Indian Curry";
+            $this->description ="Spicy and rich vegetable curry, simply irresistible";
+            $this->price = 7.25;
+            $this->extras = ["Hot Peppers", "Coconut Chutney", "Raita"];
+            break;
+        default:
+            $this->name = "Name not set";
+            $this->description ="No description";
+            $this->price = 0;
+            $this->extras = [];
+            break;
+    }
+}
     public function CalculateTotal(){
         //@todo implement method
     }
+
+
+
 }

9cfcd4a48fe89502a5dfe3c09e35816492d61026
Gordana Minovska <ekogoca@gmail.com>
2017-02-03 15:22:09 -0800

    Add font awesome

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,7 @@
     <title>Food Truck</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <!--[if lt IE 9]>

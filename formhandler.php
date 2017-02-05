9c575d175f197754fb81fcef7c50858698d214cb
monkeework <monkeework@gmail.com>
2017-02-04 18:06:25 -0800

    Refactor - Display error message IF no selctions
    seamless experience for the end user, they’re still on the page as far
    as they know/can tell.

--- formhandler.php
+++ formhandler.php
@@ -1,4 +1,13 @@
 <?php
+
+// refactor by Max -- 02/04/17
+// if no items, send back to index, flash message
+if(!isset($_POST["items"])) {
+		//send user back if empty
+		header('Location: index.php?act=error');
+}
+
+// if we pass check, load eveything
 require 'food.php';
 include 'includes/header.php';
 //echo '<pre>';
@@ -7,41 +16,22 @@ include 'includes/header.php';
 //check if the input is valid
 
 
-/*
-if(!isset($_POST["items"])) {
-    echo '<div class = "col-md-4 col-md-offset-4">
-
-              <h5 class="errorMsg">Undecided what you want to order? We suggest you order one of each!</h5>
-       <a href = "index.php">Take me back</a>';
-}
-*/
-
-
-//if the input is valid - if not send back to start
-// If user has to click 'back' they might be annoyed, best practice is redirect automatically, then inform.
-// see
-if(!isset($_POST["items"])) {
-    //send user back if empty
-    header('Location: index.php?act=error');
-}
-else {
-
 //loop through the $_POST array and create an array of Food objects the user ordered
 for ($i = 0; $i < count($_POST["items"]); $i++) {
 
-    //store object parameters from the $_POST array into variables
-    $type = $_POST["items"][$i];
-    $quantity = $_POST["quantity"][$i];
-    //check if toppings were selected
-    if(isset($_POST["topping" . $i])){
-        $toppings = $_POST["topping" . $i];
-    }
-    else{
-        $toppings = [];
-    }
+		//store object parameters from the $_POST array into variables
+		$type = $_POST["items"][$i];
+		$quantity = $_POST["quantity"][$i];
+		//check if toppings were selected
+		if(isset($_POST["topping" . $i])){
+				$toppings = $_POST["topping" . $i];
+		}
+		else{
+				$toppings = [];
+		}
 
-    /** @var array $foodOrder stores the ordered food items*/
-    $foodOrder[] = new Food($type, $quantity, $toppings);
+		/** @var array $foodOrder stores the ordered food items*/
+		$foodOrder[] = new Food($type, $quantity, $toppings);
 
 }
 
@@ -54,45 +44,45 @@ for ($i = 0; $i < count($_POST["items"]); $i++) {
 //the subtotal for each item, and a cumulative total cost due.
 $total = 0;
 foreach ($foodOrder as $food) {
-    echo '<div class = "orderSummary menuItem col-md-6 col-md-offset-3">
-              
-              <h5 class="foodName">' . $food->name . ' x ' . $food->quantity . '</h5>
-              <p class="foodName cost">$' . $food->CalculatePerItemSubtotal() . ' </p>
-              <button type="button" class="btn details"><i class="fa fa-chevron-down"></i></button>
-             <div class = "priceDetails hide" >
-              <p>Base price:(' . $food->price . ' /each)</p>
-              <p class="cost">$' . $food->CalculateBasePrice() . ' </p>';
+		echo '<div class = "orderSummary menuItem col-md-6 col-md-offset-3">
 
-    //don't display toppings price if no toppings were selected
+							<h5 class="foodName">' . $food->name . ' x ' . $food->quantity . '</h5>
+							<p class="foodName cost">$' . $food->CalculatePerItemSubtotal() . ' </p>
+							<button type="button" class="btn details"><i class="fa fa-chevron-down"></i></button>
+						 <div class = "priceDetails hide" >
+							<p>Base price:(' . $food->price . ' /each)</p>
+							<p class="cost">$' . $food->CalculateBasePrice() . ' </p>';
 
-    if($toppings != []) {
-        echo '
-              <p>+' . implode(", ", $food->toppings) . '(' . $food->CalculateToppingsCost() . ' /each) </p>
-              <p class="cost">$' . $food->CalculateToppingsCostTotal() . '  </p>';
-    }
+		//don't display toppings price if no toppings were selected
 
-    echo '
-              
+		if($toppings != []) {
+				echo '
+							<p>+' . implode(", ", $food->toppings) . '(' . $food->CalculateToppingsCost() . ' /each) </p>
+							<p class="cost">$' . $food->CalculateToppingsCostTotal() . '  </p>';
+		}
+
+		echo '
+
 <!-- added by Ayumi 2/3-->
-              <!-- <p>Subtotal before tax (' . $food->quantity . ' orders): </p>
-              <p class="cost">$' . $food->CalculateSubtotalBeforeTax() . ' </p>-->
-              
-              <p>Tax (9.6%)</p>
-              <p class="cost">$' . $food->CalculateTax() . ' </p>
-              <hr>
-              <p>Subtotal:</p>
-              <p class="cost">$' . $food->CalculatePerItemSubtotal() . ' </p>             
-              </div>
-        </div>';
-    //calculate total
+							<!-- <p>Subtotal before tax (' . $food->quantity . ' orders): </p>
+							<p class="cost">$' . $food->CalculateSubtotalBeforeTax() . ' </p>-->
 
-    $total += $food->CalculatePerItemSubtotal();
+							<p>Tax (9.6%)</p>
+							<p class="cost">$' . $food->CalculateTax() . ' </p>
+							<hr>
+							<p>Subtotal:</p>
+							<p class="cost">$' . $food->CalculatePerItemSubtotal() . ' </p>
+							</div>
+				</div>';
+		//calculate total
+
+		$total += $food->CalculatePerItemSubtotal();
 }
 
 //display total
 echo '<div id="finalPrice" class = "orderSummary menuItem col-md-6 col-md-offset-3">
-    <h5 class="total">Total price:</h5>
-    <p class="total cost">$' . number_format($total, 2) . ' </p>
-    </div>';
-}
+		<h5 class="total">Total price:</h5>
+		<p class="total cost">$' . number_format($total, 2) . ' </p>
+		</div>';
+
 include 'includes/footer.php';
--- includes/header.php
+++ includes/header.php
@@ -1,35 +1,39 @@
+<?php
+
+// 02/04/17 - added by Max
+// chek query string, if value exists, set warning text.
+if(isset($_GET["act"])) {
+		//if alert, display notice
+		$warningTxt = '<script>alert("OH NO - Something is amiss with your order, please resubmit. Thank you.")</script>';
+}else{ $warningTxt = '';}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <title>Food Truck</title>
+		<meta charset="utf-8">
+		<title>Food Truck</title>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="style.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <!--[if lt IE 9]>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <![endif]-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="style.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<!--[if lt IE 9]>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<![endif]-->
 </head>
 
 <body>
 <div class="container">
-    <h1 class="text-center">FoodTruck</h1>
-    <h4 class="text-center slogan">Feeling hungry? We got you covered!</h4>
+		<h1 class="text-center">FoodTruck</h1>
+		<h4 class="text-center slogan">Feeling hungry? We got you covered!</h4>
 
-<?php
-    // check query string for alert notice
-    $chek = $_GET['act'];
-
-    //if alert, display notice
-    if (isset($chek)){
-        echo '<script>alert("OH NO - Something is amiss with your order, please resubmit. Thank you.")</script>';
-    }
-?>
+		<? echo $warningTxt; ?>
 
 
 
-    <div class="row">
+
+		<div class="row">
 

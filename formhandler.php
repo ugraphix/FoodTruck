9daf22fb1d22f141cd1860c3480178fb1bc823d2
Gracie <grace@ugraphix.com>
2017-02-05 11:22:43 -0800

    Update formhandler.php
    Fixed - "else and elseif are on the same line as the closing brace from the earlier body."
    
    Fixed - spacing/indentation
    
    Fixed -m

--- formhandler.php
+++ formhandler.php
@@ -13,74 +13,67 @@ include 'includes/header.php';
 //check if the input is valid
 if(!isset($_POST["items"])) {
     echo '<div class = "col-md-4 col-md-offset-4">
-              
-              <h5 class="errorMsg">Undecided what you want to order? We suggest you order one of each!</h5>
-       <a href = "index.php">Take me back</a>';
-}
-else {
+    <h5 class="errorMsg">Undecided what you want to order? We suggest you order one of each!</h5>
+    <a href = "index.php">Take me back</a>';
+} else {
+    //loop through the $_POST array and create an array of Food objects the user ordered
+    for ($i = 0; $i < count($_POST["items"]); $i++) {
 
-//loop through the $_POST array and create an array of Food objects the user ordered
-for ($i = 0; $i < count($_POST["items"]); $i++) {
-
-    //store object parameters from the $_POST array into variables
-    $type = $_POST["items"][$i];
-    $quantity = $_POST["quantity"][$i];
-    //check if toppings were selected
-    if(isset($_POST["topping" . $i])){
-        $toppings = $_POST["topping" . $i];
+        //store object parameters from the $_POST array into variables
+        $type = $_POST["items"][$i];
+        $quantity = $_POST["quantity"][$i];
+        
+        //check if toppings were selected
+        if(isset($_POST["topping" . $i])) {
+            $toppings = $_POST["topping" . $i];
+        } else {
+            $toppings = [];
+        }
+        /** @var array $foodOrder stores the ordered food items*/
+        $foodOrder[] = new Food($type, $quantity, $toppings);
     }
-    else{
-        $toppings = [];
-    }
 
-    /** @var array $foodOrder stores the ordered food items*/
-    $foodOrder[] = new Food($type, $quantity, $toppings);
-
-}
-
-//create the order summary showing all the items and toppings ordered,
-//the subtotal for each item, and a cumulative total cost due.
-$total = 0;
-foreach ($foodOrder as $food) {
-    echo '<div class = "orderSummary menuItem col-md-6 col-md-offset-3">
+    //create the order summary showing all the items and toppings ordered,
+    //the subtotal for each item, and a cumulative total cost due.
+    $total = 0;
+    foreach ($foodOrder as $food) {
+        echo '<div class = "orderSummary menuItem col-md-6 col-md-offset-3">
               
-              <h5 class="foodName">' . $food->name . ' x ' . $food->quantity . '</h5>
-              <p class="foodName cost">$' . $food->CalculatePerItemSubtotal() . ' </p>
-              <button type="button" class="btn details"><i class="fa fa-chevron-down"></i></button>
-             <div class = "priceDetails hide" >
-              <p>Base price:(' . $food->price . ' /each)</p>
-              <p class="cost">$' . $food->CalculateBasePrice() . ' </p>';
+        <h5 class="foodName">' . $food->name . ' x ' . $food->quantity . '</h5>
+        <p class="foodName cost">$' . $food->CalculatePerItemSubtotal() . ' </p>
+        <button type="button" class="btn details"><i class="fa fa-chevron-down"></i></button>
+        <div class = "priceDetails hide" >
+        <p>Base price:(' . $food->price . ' /each)</p>
+        <p class="cost">$' . $food->CalculateBasePrice() . ' </p>';
 
-    //don't display toppings price if no toppings were selected
+        //don't display toppings price if no toppings were selected
+        if($toppings != []) {
+            echo '<p>+' . implode(", ", $food->toppings) . '(' . $food->CalculateToppingsCost() . ' /each) </p>
+            <p class="cost">$' . $food->CalculateToppingsCostTotal() . '  </p>';
+        }
 
-    if($toppings != []) {
         echo '
-              <p>+' . implode(", ", $food->toppings) . '(' . $food->CalculateToppingsCost() . ' /each) </p>
-              <p class="cost">$' . $food->CalculateToppingsCostTotal() . '  </p>';
+        <!-- added by Ayumi 2/3-->
+        <!-- <p>Subtotal before tax (' . $food->quantity . ' orders): </p>
+        <p class="cost">$' . $food->CalculateSubtotalBeforeTax() . ' </p>-->
+              
+        <p>Tax (9.6%)</p>
+        <p class="cost">$' . $food->CalculateTax() . ' </p>
+        <hr>
+        <p>Subtotal:</p>
+        <p class="cost">$' . $food->CalculatePerItemSubtotal() . ' </p>             
+        </div>
+        </div>';
+    
+        //calculate total
+        $total += $food->CalculatePerItemSubtotal();
     }
 
-    echo '
-              
-<!-- added by Ayumi 2/3-->
-              <!-- <p>Subtotal before tax (' . $food->quantity . ' orders): </p>
-              <p class="cost">$' . $food->CalculateSubtotalBeforeTax() . ' </p>-->
-              
-              <p>Tax (9.6%)</p>
-              <p class="cost">$' . $food->CalculateTax() . ' </p>
-              <hr>
-              <p>Subtotal:</p>
-              <p class="cost">$' . $food->CalculatePerItemSubtotal() . ' </p>             
-              </div>
-        </div>';
-    //calculate total
-
-    $total += $food->CalculatePerItemSubtotal();
-}
-
-//display total
-echo '<div id="finalPrice" class = "orderSummary menuItem col-md-6 col-md-offset-3">
+    //display total
+    echo '<div id="finalPrice" class = "orderSummary menuItem col-md-6 col-md-offset-3">
     <h5 class="total">Total price:</h5>
     <p class="total cost">$' . number_format($total, 2) . ' </p>
     </div>';
 }
+
 include 'includes/footer.php';
